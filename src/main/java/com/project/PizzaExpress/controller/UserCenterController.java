86dd13fd3575eb3a894f0f0f4c899ddf353deeac
renlvda <1041962748@qq.com>
2019-03-15 22:45:45 +0800

    【增加】用户退出登录

--- src/main/java/com/project/PizzaExpress/controller/UserCenterController.java
+++ src/main/java/com/project/PizzaExpress/controller/UserCenterController.java
@@ -2,11 +2,13 @@ package com.project.PizzaExpress.controller;
 
 import com.project.PizzaExpress.service.login.ILoginService;
 import com.project.PizzaExpress.service.login.LoginServiceImpl;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/user")
@@ -16,26 +18,27 @@ public class UserCenterController {
     private ILoginService loginService = new LoginServiceImpl();
 
     @RequestMapping("/login")
-    public int login(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password)
-    {
-        return loginService.login(username, password);
+    public int login(HttpSession session, @RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
+        int login = loginService.login(username, password);
+        if (login == 0) {
+            session.setAttribute("User", 0);
+        }
+        return login;
     }
 
     @RequestMapping("/logout")
-    public void logout()
-    {
-
+    public void logout(HttpSession session) {
+        session.invalidate();
+        return;
     }
 
     @RequestMapping("/signUp")
-    public void signUp()
-    {
+    public void signUp() {
 
     }
 
     @RequestMapping("/modify")
-    public void modify()
-    {
+    public void modify() {
 
     }
 }
